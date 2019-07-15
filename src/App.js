748f2da36f2209c8571a000e898fe327e4cff5f8
omedale <omedale@gmail.com>
2019-07-15 11:57:07 +0100

    add burger and layouts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Layout from './components/Layouts/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Nice</p>
+        <div>Nice</div>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
