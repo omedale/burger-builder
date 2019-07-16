f76dd6ca0ce9021b81368b71c0d2ebdb887638e1
omedale <omedale@gmail.com>
2019-07-16 15:44:26 +0100

    more fixes

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 import classes from './Layout.css'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = (props) => (
   <Aux>
-    <div>Toobar</div>
+    <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>
       {props.children}
     </main>
